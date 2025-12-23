e08caa24595e41a4d9774fd8606fdc302f09bb8f
AJ Kim <jkim67cloud@gmail.com>
2025-12-24 05:06:57 +0900

    feat

--- run.sh
+++ run.sh
@@ -1,8 +1,45 @@
 #!/bin/bash
 
+# 에러 발생 시 스크립트 중단 (안전장치)
+set -e
 
+BRANCH_DATE=$(LC_TIME=C date "+%b-%d-%Y" | tr '[:upper:]' '[:lower:]') # i.e) dec-24-2025
+BRANCH_NAME="${BRANCH_DATE}-daily-dive"
 
-# TODO:
-# 1. Get date like dec-23-2025-daily-dive
-# 1. get a date like 251224
-# 1. Creates a file: /proofs/meals/251224.xxxx.xxx.md
+FILE_DATE=$(date "+%y%m%d") # i.e) 251224
+FILE_PATH="proofs/meal/${FILE_DATE}.meal.md"
+
+echo "Current Date: $BRANCH_DATE"
+echo "Target Branch: $BRANCH_NAME"
+echo "Target File: $FILE_PATH"
+
+echo "Checking out main..."
+git checkout main
+git pull origin main
+
+echo "Creating branch $BRANCH_NAME..."
+git checkout -b "$BRANCH_NAME"
+
+cat <<EOF > "$FILE_PATH"
+# ${FILE_DATE}
+
+<!-- TOC -->
+
+<!-- /TOC -->
+
+## Breakfast.xxx.xx
+
+## Lunch.xxx.xx
+
+## Dinner.xxx.xx
+
+EOF
+
+echo "File created at $FILE_PATH"
+
+git add "$FILE_PATH"
+git commit -m "first automated commit"
+
+git push -u origin "$BRANCH_NAME"
+
+echo "Done! Open the PR: https://github.com/mlajkim/dive-deep/pulls"
