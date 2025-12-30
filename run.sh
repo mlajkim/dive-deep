0b2d015a2195887d5a2d3f97fdf55fd1366b33d4
AJ Kim <jkim67cloud@gmail.com>
2025-12-30 09:06:11 +0900

    feat

--- run.sh
+++ run.sh
@@ -67,6 +67,35 @@ cat <<EOF > "$FILE_PATH"
 | Protein 1/2 |   240    |   37    |
 |     ...     |   ...    |   ...   |
 
+<!--
+
+Here are the meals that you eat often:
+
+Protein:
+
+|                  Food                   | Calories | Protein |
+|:---------------------------------------:|:--------:|:-------:|
+|          Baked Chicken Breast           |   182    |   34    |
+| Protein Shake 1.5 Scoops w/ Almond Milk |   240    |   37    |
+
+Healthy Carbs (Non-Fruit):
+
+|                   Food                   | Calories | Protein |
+|:----------------------------------------:|:--------:|:-------:|
+|                Rice 100g                 |   129    |    2    |
+|               Yakiimo 180g               |   190    |    0    |
+| Topvalu Greek Yogurt with 10 blueberries | 71=63+8  |   12    |
+
+
+Fruit:
+|           Food           | Calories | Protein |
+|:------------------------:|:--------:|:-------:|
+|     One Banana 180g      |   160    |    1    |
+|     One Avocado 125g     |   200    |    3    |
+| Strawberry 1 Pack (150g) |    40    |    0    |
+
+-->
+
 EOF
 
 echo "File created at $FILE_PATH"
